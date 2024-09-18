ac7b2cd0cd1669a625a4003668bbe1835c258b40
Cilia Madani <thelab.aidevlab.com>
2024-09-18 12:16:49 +0100

    Update utility_functions.py

--- src/backend/utility_functions.py
+++ src/backend/utility_functions.py
@@ -1577,11 +1577,11 @@ def replace_short_quote_by_original(quotes_dictionnary, all_quotes_set):
         print()
         print()
         print("Quote")
-        print(quote[:50])
+        print(quote[:100])
         len_quote = len(quote)
         for full_quote in all_quotes_set:
-            print(full_quote)
-            print(full_quote[:50])
+            print("full_quote")
+            print(full_quote[:100])
             if full_quote[:len_quote] == quote:  # String slicing for comparison
                 quotes_dictionnary[full_quote] = quotes_dictionnary.pop(quote)  # Replace key
                 print("MATCH.........")
